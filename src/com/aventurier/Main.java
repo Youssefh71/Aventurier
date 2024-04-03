d73ca5815c9690371cfe477f0243c0ba20617d17
Youssefh71 <youssefhourri69@gmail.com>
2024-04-03 12:20:43 +0200

    modification gitignore

--- src/com/aventurier/Main.java
+++ src/com/aventurier/Main.java
@@ -6,6 +6,30 @@ import com.aventurier.impl.Player;
 public class Main {
     public static void main(String[] args) {
 
+        // Création d'un joueur avec les coordonnées initiales
+        Player hero1 = new Player(3, 0); // Premier test
+        Player hero2 = new Player(6, 9); // Deuxième test
+
+        DefaultMapController mapController = new DefaultMapController();
+
+        // Lecture du contenu du fichier
+        String mapContent = mapController.loadMapContent("data/carte.txt");
+
+        if (mapContent != null) {
+            // Premier test
+            System.out.println("Premier test :");
+            System.out.println("--------------------------------------------------------------------------------------------");
+            TestRunner.runTest(mapController, mapContent, hero1, "SSSSEEEEEENN");
+
+            // Deuxième test
+            System.out.println("\nDeuxième test :");
+            System.out.println("--------------------------------------------------------------------------------------------");
+            TestRunner.runTest(mapController, mapContent, hero2, "OONOOOSSO");
+        } else {
+            System.out.println("Erreur lors de la lecture du fichier de carte");
+        }
+
+
 
 
 
